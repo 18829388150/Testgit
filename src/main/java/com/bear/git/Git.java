d5d52807e3d830178f189543fa442cefe01a60d5
bear_firstgit <wutianxiong0325@qq.com>
2020-06-22 14:49:20 +0800

    test commit Git.java conflict by Testgit

--- src/main/java/com/bear/git/Git.java
+++ src/main/java/com/bear/git/Git.java
@@ -2,7 +2,7 @@ package com.bear.git;
 
 public class Git {
    public static void main(String[] args) {
-	System.out.println("Testgit 正在打印输出。。。 ");
+	System.out.println("Testgit 正在打印输出。 ");
 	
 }
 } 
